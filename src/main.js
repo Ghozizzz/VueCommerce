318594ef1315e7b05e063b333f39a240993d7617
Ghozi Izzaturrahman <ghozi98@yahoo.com>
2020-07-30 17:00:25 +0700

    First Commit, Front-end

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import jQuery from "jquery";
+import "popper.js";
+import "./assets/app.scss";
+
+window.$ = window.jQuery = jQuery;
+
+Vue.component('Navbar', require('./components/Navbar.vue').default);
 
 Vue.config.productionTip = false;
 
